4809154326f1e666da3063ec615d0cb3f4c042d2
Eduard Carreras <ecarreras@gmail.com>
2012-01-01 22:19:19 +0100

    Millores en la fitxa de la persona

--- garreta.py
+++ garreta.py
@@ -2,6 +2,17 @@
 
 from trytond.model import ModelView, ModelSQL, fields
 
+class Professio(ModelSQL, ModelView):
+    """Professio per una persona dins l'arbre Garreta.
+    """
+    _name = 'professio'
+    _description = __doc__
+
+    nom = fields.Char('Nom')
+    descripcio = fields.Text('Descripcio')
+
+Professio()
+
 class Persona(ModelSQL, ModelView):
     """Persona dins de l'arbre Garreta.
     """
@@ -17,6 +28,23 @@ class Persona(ModelSQL, ModelView):
             field_search = field_dict[persona.sexe]
             res[persona.id] = self.search([(field_search, '=', persona.id)])
         return res
+
+    def get_conjugue_actual(self, ids, name):
+        res = {}
+	matrimoni_obj = self.pool.get('matrimoni')
+	for persona in self.browse(ids):
+	    search_params = [['OR', [('persona1.id', '=', persona.id)],
+	                            [('persona2.id', '=', persona.id)],
+		             ],
+			     ('data_final', '=', False)]
+	    results = matrimoni_obj.search(search_params, limit=1, 
+	                                   order=[('data_inici', 'DESC')])
+	    if results:
+	        res[persona.id] = results[0]
+            else:
+	        res[persona.id] = False
+        return res
+
 
     def get_nom_complet(self, ids, name):
         """Retorna el nom complet.
@@ -35,26 +63,18 @@ class Persona(ModelSQL, ModelView):
     mare = fields.Many2One('persona', 'Mare', ondelete='RESTRICT')
     data_naixement = fields.Date('Data Naixement')
     lloc_naixement = fields.Char('Lloc de Naixement')
-    sexe = fields.Selection([('H', 'Home'), ('D', 'Dona')], 'Sexe')
-    fills = fields.Function(fields.One2Many('persona', None, 'Fills'),
-                            'get_fills')
-    data_obit = fields.Date('Data Obit')
-    lloc_obit = fields.Char('Lloc Obit')
+    sexe = fields.Selection([('H', 'Home'), ('D', 'Dona')], 'Sexe', required=True)
+    fills = fields.Function(fields.One2Many('persona', None, 'Fills',
+                order=[('data_naixement', 'ASC')]), 'get_fills')
+    data_obit = fields.Date(u'Data Òbit')
+    lloc_obit = fields.Char(u'Lloc Òbit')
+    professio = fields.Many2One('professio', u'Professió', ondelete='RESTRICT')
     notes = fields.Text('Notes')
-    
+    conjugue_actual = fields.Function(fields.Many2One('persona', u'Cònjugue actual'),
+        'get_conjugue_actual')
+                                       
 
 Persona()
-
-class Professio(ModelSQL, ModelView):
-    """Professio per una persona dins l'arbre Garreta.
-    """
-    _name = 'professio'
-    _description = __doc__
-
-    nom = fields.Char('Nom')
-    descripcio = fields.Text('Descripcio')
-
-Professio()
 
 class Matrimoni(ModelSQL, ModelView):
     """Unio entre dues persones.
@@ -62,8 +82,8 @@ class Matrimoni(ModelSQL, ModelView):
     _name = 'matrimoni'
     _description = __doc__
 
-    persona1 = fields.Many2One('persona', 'Conjugue 1')
-    persona2 = fields.Many2One('persona', 'Conjugue 2')
+    persona1 = fields.Many2One('persona', u'Cònjugue 1')
+    persona2 = fields.Many2One('persona', u'Cònjugue 2')
     data_inici = fields.Date('Data inici')
     data_final = fields.Date('Data final')
     notes = fields.Text('Notes')
