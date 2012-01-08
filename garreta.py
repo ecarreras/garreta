e2b60ea3928f42c4cee5f037327df16f3a7dbd57
Eduard Carreras <ecarreras@gmail.com>
2012-01-08 16:01:10 +0100

    IMP Primera versió

--- garreta.py
+++ garreta.py
@@ -7,6 +7,7 @@ class Professio(ModelSQL, ModelView):
     """
     _name = 'professio'
     _description = __doc__
+    _rec_name = 'nom'
 
     nom = fields.Char('Nom')
     descripcio = fields.Text('Descripcio')
@@ -29,6 +30,33 @@ class Persona(ModelSQL, ModelView):
             res[persona.id] = self.search([(field_search, '=', persona.id)])
         return res
 
+    def get_matrimonis(self, ids, name):
+        """Retorna els matrimonis.
+	"""
+	res = {}
+	matrimoni_obj = self.pool.get('matrimoni')
+	for persona in self.browse(ids):
+	    search_params = [['OR', [('persona1.id', '=', persona.id)],
+	                            [('persona2.id', '=', persona.id)]
+			    ]]
+	    res[persona.id] = matrimoni_obj.search(search_params)
+	return res
+
+    def set_matrimonis(self, ids, name, value):
+        matrimoni_obj = self.pool.get('matrimoni')
+        if not value:
+            return
+        for act in value:
+            print act
+            if act[0] == 'create':
+                act[1]['persona1'] = ids[0]
+                matrimoni_obj.create(act[1])
+            elif act[0] == 'write':
+                matrimoni_obj.write(act[1], act[2])
+            elif act[0] in ('delete', 'unlink'):
+                matrimoni_obj.delete(act[1])
+
+
     def get_conjugue_actual(self, ids, name):
         res = {}
 	matrimoni_obj = self.pool.get('matrimoni')
@@ -40,7 +68,11 @@ class Persona(ModelSQL, ModelView):
 	    results = matrimoni_obj.search(search_params, limit=1, 
 	                                   order=[('data_inici', 'DESC')])
 	    if results:
-	        res[persona.id] = results[0]
+		matrimoni = matrimoni_obj.browse(results[0])
+                if matrimoni.persona1.id != persona.id:		
+	            res[persona.id] = matrimoni.persona1.id
+                else:
+                    res[persona.id] = matrimoni.persona2.id
             else:
 	        res[persona.id] = False
         return res
@@ -55,6 +87,9 @@ class Persona(ModelSQL, ModelView):
 	                                     persona.nom)
 	return res
 
+    def search_nom_complet(self, name, clause):
+        return []
+
     name = fields.Function(fields.Char('Nom complet'), 'get_nom_complet')
     nom = fields.Char('Nom')
     cognom1 = fields.Char('Primer Cognom')
@@ -65,13 +100,15 @@ class Persona(ModelSQL, ModelView):
     lloc_naixement = fields.Char('Lloc de Naixement')
     sexe = fields.Selection([('H', 'Home'), ('D', 'Dona')], 'Sexe', required=True)
     fills = fields.Function(fields.One2Many('persona', None, 'Fills',
-                order=[('data_naixement', 'ASC')]), 'get_fills')
+        order=[('data_naixement', 'ASC')]), 'get_fills')
     data_obit = fields.Date(u'Data Òbit')
     lloc_obit = fields.Char(u'Lloc Òbit')
     professio = fields.Many2One('professio', u'Professió', ondelete='RESTRICT')
     notes = fields.Text('Notes')
     conjugue_actual = fields.Function(fields.Many2One('persona', u'Cònjugue actual'),
         'get_conjugue_actual')
+    matrimonis = fields.Function(fields.One2Many('matrimoni', None, 'Matrimonis',
+        order=[('data_final', 'DESC')]), 'get_matrimonis', setter='set_matrimonis')
                                        
 
 Persona()
